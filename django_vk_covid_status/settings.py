1a60f80398fe39b52c89a91f1411b03acab2619d
Denis <orex0vd@gmail.com>
2020-05-16 20:03:26 +0300

    db

--- django_vk_covid_status/settings.py
+++ django_vk_covid_status/settings.py
@@ -76,7 +76,16 @@ WSGI_APPLICATION = 'django_vk_covid_status.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'testproj.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 
 
